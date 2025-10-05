19a8092376bdf5071452144bc963badebf1243c0
Maximiliano Gonzalez <maxigonzalezwk@gmail.com>
2025-10-05 13:59:00 -0300

    Método para generar el número de la orden

--- src/main/java/com/ecommerce/spring_ecommerce/service/IOrdenService.java
+++ src/main/java/com/ecommerce/spring_ecommerce/service/IOrdenService.java
@@ -1,7 +1,11 @@
 package com.ecommerce.spring_ecommerce.service;
 
+import java.util.List;
+
 import com.ecommerce.spring_ecommerce.model.orden;
 
 public interface IOrdenService {
+    List<orden> findAll();
+
     orden save (orden orden);
 }
--- src/main/java/com/ecommerce/spring_ecommerce/service/ordenServiceImpl.java
+++ src/main/java/com/ecommerce/spring_ecommerce/service/ordenServiceImpl.java
@@ -1,5 +1,8 @@
 package com.ecommerce.spring_ecommerce.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +10,7 @@ import com.ecommerce.spring_ecommerce.model.orden;
 import com.ecommerce.spring_ecommerce.repository.IOrdenRepository;
 
 @Service
-public class ordenServiceImpl implements IOrdenService{
+public class ordenServiceImpl implements IOrdenService {
 
     @Autowired
     private IOrdenRepository ordenRepository;
@@ -15,5 +18,21 @@ public class ordenServiceImpl implements IOrdenService{
     @Override
     public orden save(orden orden) {
         return ordenRepository.save(orden);
+    }
+
+    @Override
+    public List<orden> findAll() {
+        return ordenRepository.findAll();
+    }
+
+    public String generarNumeroOrden(int num) {
+
+        String numeroOrden = String.valueOf(num);
+        int numDigitos = numeroOrden.length(); // numero de digitos
+
+        for (int j = numDigitos; j <= 9; j++)// añadimos los ceros
+            numeroOrden = "0" + numeroOrden;
+
+        return numeroOrden;
     }
 }
