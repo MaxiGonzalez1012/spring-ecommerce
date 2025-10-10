3ff3061626e057527022bcbaf13eb815760d86cc
Maximiliano Gonzalez <maxigonzalezwk@gmail.com>
2025-10-10 19:54:09 -0300

    Funcionalidad para ver los detalles de una orden

--- src/main/java/com/ecommerce/spring_ecommerce/controller/administradorController.java
+++ src/main/java/com/ecommerce/spring_ecommerce/controller/administradorController.java
@@ -2,12 +2,16 @@ package com.ecommerce.spring_ecommerce.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.ecommerce.spring_ecommerce.model.orden;
 import com.ecommerce.spring_ecommerce.model.producto;
 import com.ecommerce.spring_ecommerce.service.IOrdenService;
 import com.ecommerce.spring_ecommerce.service.IUsuarioService;
@@ -25,6 +29,8 @@ public class administradorController {
 
     @Autowired
     private IOrdenService ordenService;
+
+    private Logger log = LoggerFactory.getLogger(administradorController.class);
 
     @GetMapping("")
     public String home(Model model) {
@@ -46,5 +52,15 @@ public class administradorController {
         model.addAttribute("ordenes", ordenService.findAll());
 
         return "administrador/ordenes";
+    }
+
+    @GetMapping("/detalle/{id}")
+    public String detalle(Model model, @PathVariable Integer id) {
+        log.info("id de la orden {}", id);
+
+        orden orden = ordenService.findById(id).get();
+        model.addAttribute("detalles", orden.getDetalle());
+
+        return "/administrador/detalleorden";
     }
 }
